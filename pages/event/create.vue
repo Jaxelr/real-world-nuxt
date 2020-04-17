07e00f1bbedf0c2b4b83b96ae831182cbce557ab
Jaxel Rojas <jaxelr@gmail.com>
2020-04-17 08:55:03 -0400

    First 5 chapters

---
+++ pages/event/create.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Create an Event</h1>
+  </div>
+</template>
+<script>
+export default {
+    head() {
+      return {
+        title: 'Create an Event',
+        meta: [
+          {
+            hid: 'descr',
+            name: 'descr',
+            content:
+              'Content detail goes here'
+          }
+        ]
+      }
+    }
+}</script>
